339c5e16e6c09ac967a6caedc0e575e6d8e63263
dione-vianna <dioneg12@hotmail.com>
2022-02-14 15:29:33 -0300

    :blue_book: Imutabilidade no React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
+
+// uso de imutabilidade
+
+// sem imutabilidade
+// usuario = ['diego', 'ana', 'joao']
+// usuario.push('pedro')
+
+// usando o conceito de imutabilidade
+// novoUsuario = [...usuario, 'pedro']
+
 export function Counter() {
   // hook -> gancho
   const [count, setCount] = useState(0);
   function increment() {
     console.log('increment', count);
-    setCount(count + 1);
+    setCount(count + 1); // exemplo de imutabilidade
   }
   return (
     <div>
